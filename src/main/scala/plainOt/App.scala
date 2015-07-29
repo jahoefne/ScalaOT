a527923743a53a26e1005f4293f13af2c03f5759
jahoefne <jahoefne@htwg-konstanz.de>
2015-07-30 00:00:19 +0200

    moved experimenting code to tests

--- src/main/scala/plainOt/App.scala
+++ src/main/scala/plainOt/App.scala
@@ -1,14 +1,10 @@
 package plainOt
 
+import plainOt.PlainOT.TransformedPair
+
+
 object App {
   def main(args: Array[String]) {
-
-    val op = new PlainOT.Operation().skip(6).insert("Mama").delete(5).insert(" Mia!")
-
-    println(op.baseLength)
-
-    val applied = op.apply("hello world").get
-    println(s"'$applied' ${applied.length}  ${op.targetLength}")
 
   }
 }
